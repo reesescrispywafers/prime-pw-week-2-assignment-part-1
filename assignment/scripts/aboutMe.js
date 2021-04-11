0c2615bba496e03de5b85485cdd8a6edfd4958f1
Emry Brisky <eabrisky@gmail.com>
2021-04-11 16:22:38 -0500

    final commit

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -90,5 +90,6 @@ switch (pets >= friendsPets)
 }
 console.log(mostPets);
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+// :)
 
 adventurous == true ? console.log("Adventures are great!") : console.log("How about we stay home?");
